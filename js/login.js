914338fa9fc01cd7ffd3f9e2604b11b38e3c034f
Alisson Peixer <48291580+alissonpeixer@users.noreply.github.com>
2022-08-01 00:52:48 -0300

    :rocket:

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 const userLoged = localStorage.getItem('token');
   if(userLoged){
-    window.location.href = "";
+    window.location.href = "https://alissonpeixer.github.io/anotacao-ui/notes.html";
   }
 
 
