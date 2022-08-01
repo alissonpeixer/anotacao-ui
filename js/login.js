32769658f943917ed10cb8dbec57c4bb0b248c2f
Alisson Peixer <48291580+alissonpeixer@users.noreply.github.com>
2022-08-01 00:51:49 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 const userLoged = localStorage.getItem('token');
   if(userLoged){
-    window.location.href = "https://alissonpeixer.github.io/anotacao-ui/notes.html";
+    window.location.href = "";
   }
 
 
